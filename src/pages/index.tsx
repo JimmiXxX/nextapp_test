89dd8fbad91f5720d4ff52234472d915d52ab1c7
JimmiXxX <jimmix139@gmail.com>
2024-01-24 11:58:35 +0300

    Исправление ошибок и изменение структры

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ const HomePage = () => {
             <div className={ styles.navBar }>
                 <Link href='/List/1' className={ styles.link }>Первая страница</Link>
                 <Link href='/List/2' className={ styles.link }>Вторая страница</Link>
-                <Link href='/List/3' className={ styles.link }>3 страница</Link>
             </div>
         </div>
     )
